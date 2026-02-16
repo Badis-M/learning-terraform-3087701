be89e6434764ecf074eaad65b57f5dc7dd38c9cc
Badis Merakchi <badis.merakchi@gmail.com>
2026-02-16 11:50:03 +0100

    typoencore

--- main.tf
+++ main.tf
@@ -93,7 +93,7 @@ module "blog_autoscaling" {
   image_id             = data.aws_ami.app_ami.id
 
   traffic_source_attachments = {
-    blog_alb = {
+    blog-alb = {
       traffic_source_identifier = aws_lb_target_group.blog.arn
     }
   }
