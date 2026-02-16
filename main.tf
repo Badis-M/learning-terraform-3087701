4ab357289106aee755ff5a358aa45ca303df1fc4
Badis Merakchi <badis.merakchi@gmail.com>
2026-02-16 11:58:48 +0100

    fix

--- main.tf
+++ main.tf
@@ -70,10 +70,11 @@ module "blog_alb" {
   }
 }
 
-resource "aws_lb_target_group_attachment" "blog" {
-  target_group_arn = aws_lb_target_group.blog.arn
-  target_id        = aws_instance.blog.id
-  port             = 80
+resource "aws_lb_target_group" "blog" {
+  name = "blog"
+  port = 80
+  protocol = "HTTP"
+  vpc_id = module.blog_vpc.vpc_id
 }
 
 module "blog_autoscaling" {
